62a8987ef6ee2e910f3f236504b714709c97e731
Gerhard R <gerd.r.devel@googlemail.com>
2013-08-28 17:16:58 +0200

    Fix more warnings

--- src/6model/reprs.c
+++ src/6model/reprs.c
@@ -130,6 +130,7 @@ static MVMObject * default_at_key_boxed(MVMThreadContext *tc, MVMSTable *st, MVM
 }
 GCC_DIAG_ON(return-type)
 static void default_bind_key_ref(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key, void *value_addr) {
+    (void)root, (void)data, (void)key, (void)value_addr;
     die_no_ass(tc, st->REPR->name);
 }
 static void default_bind_key_boxed(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key, MVMObject *value) {
@@ -138,10 +139,12 @@ static void default_bind_key_boxed(MVMThreadContext *tc, MVMSTable *st, MVMObjec
 }
 GCC_DIAG_OFF(return-type)
 static MVMint64 default_exists_key(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key) {
+    (void)root, (void)data, (void)key;
     die_no_ass(tc, st->REPR->name);
 }
 GCC_DIAG_ON(return-type)
 static void default_delete_key(MVMThreadContext *tc, MVMSTable *st, MVMObject *root, void *data, MVMObject *key) {
+    (void)root, (void)data, (void)key;
     die_no_ass(tc, st->REPR->name);
 }
 GCC_DIAG_OFF(return-type)
@@ -152,6 +155,7 @@ GCC_DIAG_ON(return-type)
 
 /* Set default attribute functions on a REPR that lacks them. */
 static void add_default_attr_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
+    (void)tc;
     repr->attr_funcs = malloc(sizeof(MVMREPROps_Attribute));
     repr->attr_funcs->get_attribute = default_get_attribute;
     repr->attr_funcs->bind_attribute = default_bind_attribute;
@@ -161,6 +165,7 @@ static void add_default_attr_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
 
 /* Set default boxing functions on a REPR that lacks them. */
 static void add_default_box_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
+    (void)tc;
     repr->box_funcs = malloc(sizeof(MVMREPROps_Boxing));
     repr->box_funcs->set_int = default_set_int;
     repr->box_funcs->get_int = default_get_int;
@@ -173,6 +178,7 @@ static void add_default_box_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
 
 /* Set default positional functions on a REPR that lacks them. */
 static void add_default_pos_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
+    (void)tc;
     repr->pos_funcs = malloc(sizeof(MVMREPROps_Positional));
     repr->pos_funcs->at_pos = default_at_pos;
     repr->pos_funcs->bind_pos = default_bind_pos;
@@ -188,6 +194,7 @@ static void add_default_pos_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
 
 /* Set default associative functions on a REPR that lacks them. */
 static void add_default_ass_funcs(MVMThreadContext *tc, MVMREPROps *repr) {
+    (void)tc;
     repr->ass_funcs = malloc(sizeof(MVMREPROps_Associative));
     repr->ass_funcs->at_key_ref = default_at_key_ref;
     repr->ass_funcs->at_key_boxed = default_at_key_boxed;
